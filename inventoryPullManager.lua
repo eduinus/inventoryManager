6a67ce3fca6d1b4ec23633c14c490ef7a861636f
eduinus <5422595+eduinus@users.noreply.github.com>
2019-11-10 08:29:57 -0500

    Update inventoryPullManager.lua

--- inventoryPullManager.lua
+++ inventoryPullManager.lua
@@ -1,4 +1,7 @@
 -sit on charger until server says to go get some stuff
+-(item type, quantity, location/(s))
 -go get it
+-put in chest
+return to charger
 
 -if server says to do a full accounting, do that
