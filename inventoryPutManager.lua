d6adf9a1ad2895e5820525e4b9042f1ce88c97ac
eduinus <5422595+eduinus@users.noreply.github.com>
2019-11-10 08:33:06 -0500

    Update inventoryPutManager.lua

--- inventoryPutManager.lua
+++ inventoryPutManager.lua
@@ -1,6 +1,6 @@
 --sit on charger until something is in chest
 --load up on as much of it as you can
---pass item name to robot, get right location(s) (in case you need to place some in a new chest b/c old one will be full)
+--pass item name to robot and quantity, get right location(s) (in case you need to place some in a new chest b/c old one will be full)
 --return to charger
 
 --if sitting on chest and power low, charge
