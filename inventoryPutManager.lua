991d1f9fdb404b57e6e71bfd1a966a0019bb6ce6
eduinus <5422595+eduinus@users.noreply.github.com>
2019-11-10 08:23:01 -0500

    Update and rename inventoryManager.lua to inventoryPutManager.lua

---
+++ inventoryPutManager.lua
@@ -0,0 +1,5 @@
+--sit on charger until something is in chest
+--pass item name to robot, get right location(s) (in case you need to place some in a new chest b/c old one will be full)
+--
+
+--if sitting on chest and power low, charge
