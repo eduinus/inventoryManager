f54f549e015e6b1043c4064ade55a343b35b6e2b
eduinus <5422595+eduinus@users.noreply.github.com>
2019-11-11 05:09:32 -0500

    Update inventoryPutManager.lua

--- inventoryPutManager.lua
+++ inventoryPutManager.lua
@@ -1,4 +1,3 @@
-Rewrite for robots? - which is faster given inventory constraints and base speed?
 actually put all this in server. Just have drones take commands.
 also, move shit to center of storage room!
 
