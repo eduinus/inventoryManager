142f45d07147c7e8176794d650bda93427f737a5
eduinus <5422595+eduinus@users.noreply.github.com>
2019-11-12 06:39:59 -0500

    Update inventoryServer.lua

--- inventoryServer.lua
+++ inventoryServer.lua
@@ -1,3 +1,35 @@
 -record Inventory mappings
 -serve inventory Qs to drones, viewing pc etc.
 -give commands to put drone, pull drone
+
+Putter: actually put all this in server. Just have drones take commands.
+also, move shit to center of storage room!
+
+--get server ok to run?
+while oktorun
+  if energylow then
+    os.sleep(5)
+    print("chargin... "..energy)
+  elseif there's something in the chest
+    suck up as much of it as you can
+    pass item name and quantity to server, get right locations (in case you need to fill 2)
+    go to addresses and dump the stuff
+    return to charger
+  end
+end
+
+Puller: actually put all this in server. Just have drones take commands.
+also, move shit to center of storage room!
+
+--get server ok to run?
+while oktorun
+  if energylow then
+    os.sleep(5)
+    print("chargin... "..energy)
+  elseif there's something in the chest
+    suck up as much of it as you can
+    pass item name and quantity to server, get right locations (in case you need to fill 2)
+    go to addresses and dump the stuff
+    return to charger
+  end
+end
