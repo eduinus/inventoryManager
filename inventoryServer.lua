f2a29f775aeac37580059917d35d4bdda96dd394
eduinus <5422595+eduinus@users.noreply.github.com>
2019-11-27 04:15:49 -0500

    Update inventoryServer.lua

--- inventoryServer.lua
+++ inventoryServer.lua
@@ -213,7 +213,8 @@ while continue do
   if dr(pushPort, "computer.maxEnergy()*0.1 < computer.energy()") then -- if drone doesn't need to charge...
     foundItem = nil
     sucks = 0
-    slotx = tonumber(dr(pushPort, "ic.getInventorySize(3)"))
+    slotx = dr(2412, "ic.getInventorySize(3)")
+    print(slotx)
     for slot = slotx, 1, -1 do
       item = dr(pushPort, "ic.getStackInSlot(3,"..slot..")")
       if foundItem == nil and item ~= nil then
