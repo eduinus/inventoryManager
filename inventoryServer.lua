bc6ca1e93558d45a57bd061063913e67b1a1d395
eduinus <5422595+eduinus@users.noreply.github.com>
2020-04-08 16:19:55 -0400

    Update inventoryServer.lua

--- inventoryServer.lua
+++ inventoryServer.lua
@@ -1,3 +1,12 @@
 local event = require("event")
 local modem = component.modem
 local term = require("term")
+
+- serve remote items search requests
+- serve remote item requests
+- serve local item search requests
+- serve local item requests
+- store items in chest
+- render item inventory graphically when not in use
+- also render recent inventory changes
+- button for quitting
