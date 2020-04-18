1cf2033c80626234455e19495cbc5fb06c672acd
eduinus <5422595+eduinus@users.noreply.github.com>
2020-04-18 13:10:56 -0400

    Update inventoryServer.lua
    removed borrowed code, rewrote :100:

--- inventoryServer.lua
+++ inventoryServer.lua
@@ -1,12 +1,53 @@
 local event = require("event")
 local component = require("component")
 local modem = component.modem
-local ct = component.transposer
+local transposer = component.transposer
 local term = require("term")
 local sides = require("sides")
 
+-- CHANGE THESE BASED ON YOUR SETUP
+local storageSide = sides.down
+local localChest = sides.up
+local remoteChest = sides.front
+local bufferChest = sides.back
+-- END CONFIG
+
+function tableLength(table)
+  count = 1
+  while table[count] ~= nil do
+    count=count+1
+  end
+  return count-1
+end
+
+function search(searchTerm)
+-- searches for an item based on searchTerm
+-- returns a table of items with their location, and all of their information per ocdoc.cil.li/component:inventory_controller
+searchResults = {}
+local count = 0
+  for i = 1, (transposer.getInventorySize(storageside)) do
+    local item = transposer.getStackInSlot(storageside, i)
+    if item then
+      if string.find(item.name .. "^" .. item.label .. "^" .. item.id .. "^", searchTerm) then
+        count = count + 1
+        searchResults[count] = item -- add item to searchResults table
+        searchResults[count][-1] = i -- add location of item to -1th column in table
+      end
+    end
+  end
+  return searchResults
+end
+
+function retrieve(itemLocation, quantity, destination)
+-- pulls items at itemLocation in quantity given
+  
+end
+
 while true do
-  local id, _, x, y = event.pullMultiple("interrupted", "modem_message", "key_down")
+  local id, arg1, arg2, arg3, arg4, arg5 = event.pullMultiple("interrupted", "modem_message", "key_down")
+  -- for modem_message: id, localNetworkCard, remoteAddress, port, distance, payload
+  -- for key_down: id, keyboardAddress, char, code, playerName
+  -- for interrupted: id, ...
 
   if id == "interrupted" then -- this means you pressed ctrl + c
     print("soft interrupt, closing")
@@ -15,56 +56,14 @@ while true do
   elseif id == "modem_message" then -- this means we have some sort of remote query
     print("user clicked", x, y)
     -- serve remote items search requests
-    -- serve remote item requests
+    -- serve remote item requests, but hold program until items taken for X time.
 
-  elseif id == "touch" then -- this means we have some sort of local query
-    print("key_down", x, y)
+  elseif id == "key_down" then -- this means we have some sort of local query
     -- serve local item search requests
     -- serve local item requests
-
-    -- If given an argument search attached storage (storageside) for items and if
-    --   one match pull upto 64 items into the attached chest (chestside)
-    --   If multiple items lists the items, give slot number to pull from that
-    --   slot
-    --   If no arguments return all items in chest (chestside)
-    local storageside = sides.down -- change as necessary
-    local chestside = sides.up -- change as necessary
-
-    local args, opts = shell.parse(...) -- kill this
-
-    if args[1] then
-        local searchfor = args[1]
-
-        local counted = 0
-        if not tonumber(searchfor) then
-            local i = 0
-            for i =1, (ct.getInventorySize(storageside)) do
-                local item = ct.getStackInSlot(storageside, i)
-
-                if item then
-                    if string.find(item.name .. "^" .. item.label .. "^", searchfor) then
-                        print(i .. ": " .. item.name .. " - " .. item.label .. " (" .. item.size .. ")")
-                        counted = counted + 1
-                        slot = i
-                    end
-                end
-            end
-        else
-            slot = tonumber(searchfor)
-            counted = 1
-        end
-
-        if counted == 1 then
-            ct.transferItem(storageside, chestside, 64, slot)
-        end
-    else  -- No arguments indicate send everything back
-        for i = ct.getInventorySize(chestside), 1, -1 do
-            ct.transferItem(chestside, storageside, 64, i)
-        end
-    end
-
   end
+
   -- now store any items in buffer chest
-  -- render item inventory graphically when not in use if you stored items in the above chest! (only if so)
+  -- render item inventory graphically when not in use if you stored items from buffer chest OR if items were removed from buffer chest
   -- note recent inventory changes
 end
