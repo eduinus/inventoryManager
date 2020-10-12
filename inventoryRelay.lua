051479822149ee950325a78ef41738314c6f9397
eduinus <5422595+eduinus@users.noreply.github.com>
2020-10-11 20:52:11 -0400

    old local changes

--- inventoryRelay.lua
+++ inventoryRelay.lua
@@ -3,11 +3,14 @@ local modem = component.modem
 local event = require("event")
 local serialization = require("serialization")
 
--- CHANGE THESE BASED ON YOUR SETUP
-modem.open(54978) -- max port is 65535
---modem.open()
---modem.open()
---modem.open()
+-- BEGIN CONFIG
+modem.open(54978) -- ports to robots etc. go here - max port is 65535
+-- modem.open()
+-- modem.open()
+-- modem.open()
+
+
+
 -- END CONFIG
 
 function tableLength(table) -- presumes table index begins at 1
@@ -18,14 +21,29 @@ function tableLength(table) -- presumes table index begins at 1
   return count
 end
 
-local id, localNetworkCard, remoteAddress, port, distance, payload = event.pull("modem_message")
-  -- handle genSearch
-  -- handle specSearch
-  -- handle requests
+while true do
+  local id, localAddress, remoteAddress, port, distance, payload = event.pull("modem_message")
+  -- for modem_message: id, localAddress, remoteAddress, port, distance, payload
+
+  -- localAddress is the address of the modem component the message was received by.
+  -- remoteAddress is the address of the network card the message was sent from.
+  -- port is the port number the message was delivered to.
+
+  modem.broadcast(port, payload)
+
+
+  -- handle genSearch?    - pass to main Server, pass table back
+    -- modem.send(remoteAddress, port, serialization.serialize(genSearch(payLoad[2])))
+
+  -- handle specSearch?   - pass to main Server, pass table back
+    -- modem.send(remoteAddress, port, serialization.serialize(specSearch(payLoad[2], payLoad[3], payLoad[4], payLoad[5], payLoad[6], payLoad[7], payLoad[8], payLoad[9])))
+
+  -- handle requests?     - pass to main Server, pass success etc. back
+    -- modem.send(remoteAddress, port, "noGrab") -- if robot doesn't take it out of the transciever
+    -- modem.send(remoteAddress, port, "done") -- when request is done and one item was found matching request
+    -- modem.send(remoteAddress, port, "moreThanOneSuchItem") -- request is done but more than one item was found, so not sending them.
+    -- modem.send(remoteAddress, port, "noSuchItem") -- request done because no items found matching description.
+
 end
 
--- picks up on robot or remote item requests / searches
--- stores these queries in array (deletes duplicative requests)
--- passes them to server periodically, and then removes them when da server says it's handling it
-
--- Network setup: linked network cards in server rack, ran with network card to server
+-- multiple modems? https://ocdoc.cil.li/component:component_access https://oc.cil.li/topic/1532-how-to-separate-multiple-components-of-the-same-name-in-code/
--- inventoryServer.lua
+++ inventoryServer.lua
@@ -7,13 +7,13 @@ local sides = require("sides")
 local serialization = require("serialization")
 local keyboard = require("keyboard")
 
--- CHANGE THESE BASED ON YOUR SETUP
+-- BEGIN CONFIG
 local storageSide = sides.down
 local localChest = sides.up
 local remoteChest = sides.front
 local bufferChest = sides.back
 
-modem.open(54978) -- max port is 65535
+modem.open(54978) --  ports to all relays and robots should go here - max port is 65535
 --modem.open()
 --modem.open()
 --modem.open()
@@ -22,16 +22,16 @@ modem.open(54978) -- max port is 65535
 function tableLength(table) -- presumes table index begins at 1
   count = 0
   while table[count] ~= nil do
-    count=count+1
+    count = count + 1
   end
   return count
 end
 
 function genSearch(searchTerm)
--- searches for an item based on searchTerm
--- returns a table of items with their location, and all of their information per ocdoc.cil.li/component:inventory_controller
-local searchResults = {}
-local count = 0
+  -- searches for an item based on searchTerm
+  -- returns a table of items with their location, and all of their information per ocdoc.cil.li/component:inventory_controller
+  local searchResults = {}
+  local count = 0
   for i = 1, transposer.getInventorySize(storageSide) do
     local item = transposer.getStackInSlot(storageSide, i)
     if item then
@@ -39,7 +39,6 @@ local count = 0
         count = count + 1
         searchResults[count] = item -- add item to searchResults table
         searchResults[count][-1] = i -- add location of item to -1th column in table
-        searchResults[count][-2] = transposer.getSlotStackSize(storageSide, i) -- add quantity of item to -2th column in table
       end
     end
   end
@@ -47,11 +46,11 @@ local count = 0
 end
 
 function specSearch(damage, maxDamage, size, maxSize, id, name, label, hasTag)
--- searches for an item based on provided categories
--- returns a table of items with their location, and all of their information per ocdoc.cil.li/component:inventory_controller
-if damage == nil and maxDamage == nil and size == nil and maxSize == nil and id == nil and name == nil and label == nil and hasTag == nil then return false end
-local searchResults = {}
-local count = 0
+  -- searches for an item based on provided categories
+  -- returns a table of items with their location, and all of their information per ocdoc.cil.li/component:inventory_controller
+  if damage == nil and maxDamage == nil and size == nil and maxSize == nil and id == nil and name == nil and label == nil and hasTag == nil then return false end
+  local searchResults = {}
+  local count = 0
   for i = 1, transposer.getInventorySize(storageSide) do
     local item = transposer.getStackInSlot(storageSide, i)
     if item then
@@ -66,14 +65,13 @@ local count = 0
         count = count + 1
         searchResults[count] = item -- add item to searchResults table
         searchResults[count][-1] = i -- add location of item to -1th column in table
-        searchResults[count][-2] = transposer.getSlotStackSize(storageSide, i) -- add quantity of item to -2th column in table
       end
     end
   end
   return searchResults
 end
 
-while true do
+while true do -- MAIN LOOP
   local id, arg1, arg2, arg3, arg4, arg5 = event.pullMultiple("interrupted", "modem_message", "key_down")
   -- for modem_message: id, localNetworkCard, remoteAddress, port, distance, payload
   -- for key_down: id, keyboardAddress, char, code, playerName
@@ -135,16 +133,16 @@ while true do
         for i=1, tableLength(resultsTable) do
           for o=1, 8 do
             print(resultsTable[i][o])
-            for i=1, (15 - string.len(resultsTable[i][o])) do io.write("-") end
+            for e=1, (15 - string.len(resultsTable[i][o])) do io.write(" ") end
           end
-            print(resultsTable[i][-1])
-            for i=1, (15 - string.len(resultsTable[i][-1])) do io.write("-") end
-            print(resultsTable[i][-2])
-            for i=1, (15 - string.len(resultsTable[i][-2])) do io.write("-") end
+          print(resultsTable[i][-1])
+          for e=1, (15 - string.len(resultsTable[i][-1])) do io.write(" ") end
+          print(resultsTable[i].size)
+          for e=1, (15 - string.len(resultsTable[i].size)) do io.write(" ") end
         end
         print("Request? (enter location #) or Search Again? (S)")
         local itemLocation = io.read()
-        if itemLocation ~= "S" and userInput ~= "s" then
+        if itemLocation ~= "S" and itemLocation ~= "s" then
           print("Quantity?")
           local itemQuantity = io.read()
           transposer.transferItem(storageSide, localChest, itemQuantity, itemLocation, 1)
@@ -161,7 +159,7 @@ while true do
 
   for i=1, transposer.getInventorySize(bufferChest) do -- now store any items in buffer chest
     if transposer.getStackInSlot(bufferChest, i) ~= nil then
-      transposer.transferItem(bufferChest, storageSide, 64, i)
+      transposer.transferItem(bufferChest, storageSide, 999, i)
       print("-> stored items from bufferChest")
     else
       break
